d316fe00d41158e96705d70ba347b9e7bc3ec228
Sergio Gallegos <isc.sergio.g@gmail.com>
2020-09-22 23:39:25 -0500

    Actualización v-if
    Se actualiza repositorio para un loop con variables anidadas

--- Arrays_condicionantes/app.js
+++ Arrays_condicionantes/app.js
@@ -8,11 +8,30 @@
         author: 'J.R.R. Tolkiens',
         books: '3'
       },
-      testData: {
+      testData2:  {
         name: 'TESTOBJECT', 
-        id: 10,
+        id: 3,
         data: [1.67, 1.33, 0.98, 2.21]
       },
+      testloop: [ {
+        name: 'TESTOBJECT', 
+        id: 3,
+        data: [1.67, 1.33, 0.98, 2.21], 
+        sh: false
+      },
+      {
+        name: 'TESTOBJECT2', 
+        id: 9,
+        data: [5,6,7,8], 
+        sh: false
+      },
+      {
+        name: 'TESTOBJECT3', 
+        id: 10,
+        data: [10,11,12,13], 
+        sh: false
+      },
+    ],
       toggle1: true
     }
   });
